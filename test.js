787d02e8cab8d034b1bb109b85b3fb6db07a9313
Noir <haydnady@gmail.com>
2020-10-28 21:51:53 -0400

    Initital commit of practice test script

---
+++ test.js
@@ -0,0 +1,18 @@
+// Values and Variables
+let country = "USA"
+let continent = "North America"
+let population = "999999999"
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+// Data Types
+let isIsland = false
+let language
+
+console.log(typeof(isIsland))
+console.log(typeof(population))
+console.log(typeof(country))
+console.log(typeof(language))
+console.log(typeof(language))
