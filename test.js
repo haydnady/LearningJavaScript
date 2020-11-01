43e4ca81bcaa31cce44683c36b1764f709c016f0
Noir <haydnady@gmail.com>
2020-11-01 12:30:53 -0500

    Updated with bmi calculations

--- test.js
+++ test.js
@@ -3,16 +3,31 @@ let country = "USA"
 let continent = "North America"
 let population = "999999999"
 
+/*
 console.log(country)
 console.log(continent)
 console.log(population)
+*/
 
 // Data Types
 let isIsland = false
 let language
-
+/*
 console.log(typeof(isIsland))
 console.log(typeof(population))
 console.log(typeof(country))
 console.log(typeof(language))
 console.log(typeof(language))
+*/
+
+// Calculate BMI
+const johnsHeight = 1.95;
+const johnsMass = 92;
+const marksHeight = 1.69;
+const marksMass = 78;
+
+let johnsBMI = johnsMass / johnsHeight **2 / (johnsHeight * johnsHeight);
+let marksBMI = marksMass / marksHeight **2 / (marksHeight * marksHeight);
+
+let markHigherBMI = (marksBMI > johnsBMI)
+console.log("Mark has higher BMI than John:", markHigherBMI)
