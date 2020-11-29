ed474f72dcb28b309d2e8f393c2ee3095412d307
Haydn <haydnady@gmail.com>
2020-11-29 13:12:29 -0500

    Added checkWinner func

--- part2/part2.js
+++ part2/part2.js
@@ -31,6 +31,35 @@ const calcAge = function (birthYear)
 console.log(`Your age is ${calcAge(2018)}`);
 
 
-// Special function expression
+// "Special" arrow function expression
 const calcAge2 = birthYear => 2037 - birthYear;
 console.log(`Your age is ${calcAge2(2015)}`);
+
+
+// Testing
+const calcAverage = (valOne, valTwo, valThree) => (valOne + valTwo + valThree) / 3;
+let avgDolphins = calcAverage(85, 54, 41);
+console.log(`The Dolphins average score is: ${avgDolphins}`);
+
+let avgKoalas = calcAverage(23, 34, 27);
+console.log(`The Koalas average score is: ${avgKoalas}`);
+
+
+// Check winner (winner needs to have double points than other team)
+function checkWinner(dataOne, dataTwo, teamOneName, teamTwoName)
+{
+    if (dataOne >= dataTwo*2)
+    {
+        console.log(`The winner is ${teamOneName} (${dataOne} vs. ${dataTwo}) 🏆`);
+    }
+    else if (dataTwo >= dataOne*2)
+    {
+        console.log(`The winner is ${teamTwoName} (${dataTwo} vs. ${dataOne}) 🏆`);
+    }
+    else
+    {
+        console.log(`The winner is no one 😂`);
+    }
+}
+
+checkWinner(avgDolphins, avgKoalas, "Dolphins", "Koalas");
