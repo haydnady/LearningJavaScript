0ec0c1712d6816eb04226ad0b475f458cefdcc64
Haydn <haydnady@gmail.com>
2020-12-05 16:25:19 -0500

    printFocast function created

--- devSkills/devSkills.js
+++ devSkills/devSkills.js
@@ -5,6 +5,27 @@ const myObject = {
     "1": "One",
     "2": "Two",
     "3": "Three"
+};
+
+console.table(myObject);
+
+function printForcast(theArray)
+{
+    for (var i = 0; i < theArray.length; i++)
+    {
+        if (i == 0)
+        {
+            console.log(`... ${theArray[0]}C in ${i+1} days`);
+        }
+        else if ((i+1) == theArray.length)
+        {
+            console.log(`... ${theArray[-1]}C in ${i+1} days`);
+        }
+        else
+        {
+            console.log(`... ${theArray[i]}C in ${i+1} days ...`);
+        }
+    }
 }
 
-console.table(myObject)
+console.log(printForcast([17, 21, 23]));
