cf8c4aac7482ac2419043ee9dcc8240d9e6ed6a6
Haydn <haydnady@gmail.com>
2021-02-06 18:20:12 -0500

    Game logic WIP

--- GuessMyNumber/starter/script.js
+++ GuessMyNumber/starter/script.js
@@ -1,1 +1,30 @@
-'use strict';
+"use strict";
+
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+// Function for when button is clicked
+document.querySelector(".check").addEventListener
+("click", function () 
+{
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess)
+
+    if (!guess)
+    {
+        document.querySelector(".message").textContent = "⛔ No number!";
+    }
+    else if (guess === secretNumber)
+    {
+        document.querySelector(".message").textContent = "🎆 Yay you won!";
+    }
+    else if (guess > secretNumber)
+    {
+        document.querySelector(".message").textContent = "📈 Too high!";
+    }
+    else if (guess < secretNumber)
+    {
+        document.querySelector(".message").textContent = "📉 Too low!";
+    }
+});
