4b60c09eca917bbe04b07fe0b1d535b4484e0809
Haydn <haydnady@gmail.com>
2021-02-06 19:07:54 -0500

    Added again button functionality

--- GuessMyNumber/starter/script.js
+++ GuessMyNumber/starter/script.js
@@ -1,30 +1,61 @@
 "use strict";
 
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+console.log(`The secret number is: ${secretNumber}`)
 
-// Function for when button is clicked
+
+// Function for when check button is clicked
 document.querySelector(".check").addEventListener
 ("click", function () 
 {
     const guess = Number(document.querySelector(".guess").value);
-    console.log(guess)
 
-    if (!guess)
+    if (score <= 0)
     {
-        document.querySelector(".message").textContent = "⛔ No number!";
+        document.querySelector(".message").textContent = "😿 Game Over!";
+        document.querySelector(".score").textContent = score;
     }
-    else if (guess === secretNumber)
+    else
     {
-        document.querySelector(".message").textContent = "🎆 Yay you won!";
+        if (!guess)
+        {
+            document.querySelector(".message").textContent = "⛔ No number!";
+        }
+        else if (guess === secretNumber)
+        {
+            document.querySelector(".message").textContent = "🎆 Yay you won!";
+            document.querySelector("body").style.backgroundColor = "#60b347";
+            document.querySelector(".number").style.width = "30rem";
+            document.querySelector(".number").textContent = score;
+        }
+        else if (guess > secretNumber)
+        {
+            document.querySelector(".message").textContent = "📈 Too high!";
+            document.querySelector(".score").textContent = score--;
+        }
+        else if (guess < secretNumber)
+        {
+            document.querySelector(".message").textContent = "📉 Too low!";
+            document.querySelector(".score").textContent = score--;
+        }
     }
-    else if (guess > secretNumber)
-    {
-        document.querySelector(".message").textContent = "📈 Too high!";
-    }
-    else if (guess < secretNumber)
-    {
-        document.querySelector(".message").textContent = "📉 Too low!";
-    }
+});
+
+
+// Function for when again button is clicked
+document.querySelector(".again").addEventListener
+("click", function () 
+{
+    score = 20
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    console.log(`The secret number is: ${secretNumber}`)
+
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".guess").textContent = "";
+    document.querySelector(".message").textContent = "Start guessing...";    
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector(".number").textContent = "?";
 });
