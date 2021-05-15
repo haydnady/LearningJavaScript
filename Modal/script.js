e4016e9a4a0f1cf73272b46610349ce28c2fbf71
Haydn <haydnady@gmail.com>
2021-05-15 19:26:33 -0400

    Added button functionality

--- Modal/script.js
+++ Modal/script.js
@@ -5,15 +5,21 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-console.log(btnsOpenModal);
+// console.log(btnsOpenModal);
 
 // Click event for all modals.
 for (let i = 0; i < btnsOpenModal.length; i++)
 {
-    btnsOpenModal[i].addEventListener("click", function () 
+    btnsOpenModal[i].addEventListener("click", function() 
     {
         console.log("Button Clicked!");
         modal.classList.remove("hidden");
         overlay.classList.remove("hidden");
     });
 }
+
+btnCloseModal.addEventListener("click", function()
+{
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+});
